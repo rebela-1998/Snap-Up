742f9e6c174ac3c3a10a30cb8744568af088e587
rebela-1998 <rebela.ray@outlook.com>
2024-03-12 00:12:06 +0530

    added reducers in cart

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -1,9 +1,46 @@
 import './Cart.css';
-import {  useSelector } from 'react-redux';
+import {  useSelector,useDispatch } from 'react-redux';
+import { increaseItemQty,decreaseItemQty,removeItem } from '../Store/Slices/CartSlice';
 
 const Cart = () => {
   const items = useSelector((state) => state.cart)
-  
+  const dispatch=useDispatch();
+
+  const increaseQty = (id) => {
+    dispatch(increaseItemQty(id));
+  }
+
+  const decreaseQty = (id) => {
+    dispatch(decreaseItemQty(id));
+  }
+  const removeFromCart = (product) => {
+    dispatch(removeItem(product));
+  }
+
+  const addedQty=(id)=>{
+    for(let i of items){
+      if(i.product_id===id)
+        return i.quantity;
+     }
+  }
+
+  const totalPrice=(id,price)=>{
+    if(addedQty(id)==1){
+      return price
+    }
+    else{
+      return price*addedQty(id)
+    }
+  }
+
+  const checkoutAmnt=()=>{
+    let totalAmount=0;
+    for(let i of items){
+      totalAmount=totalAmount+Number(totalPrice(i.product_id,i.product_price))
+    }
+    return totalAmount
+  }
+
   return (
     <>
       {items.length==0?
@@ -11,7 +48,7 @@ const Cart = () => {
       :
       <div className='added-items'>
         <div className="checkout-box">
-          <p className="Total-items">Subtotal ({items.length} items): Rs: {items.product_price}</p>
+          <p className="Total-items">Subtotal ({items.length} items): Rs: {checkoutAmnt()}</p>
           <button className="checkout" href='/' type="submit">Checkout</button>
         </div>
         <div className="itms" >
@@ -21,11 +58,15 @@ const Cart = () => {
               <p className="itmName">{item.product_name}</p>
               <p className='itmwt'>{item.product_unit}</p>
               <div className="qty">
-                <button type="button" className="qty_update">-</button>
-                <p className='final_qty'>1</p>
-                < button type="button" className="qty_update">+</button>
+                <button type="button" className="qty_update" onClick={() => {
+                          addedQty(item.product_id)==1?
+                          removeFromCart(item)
+                          :
+                          decreaseQty(item.product_id)}}>-</button>
+                <p className='final_qty'>{addedQty(item.product_id)}</p>
+                < button type="button" className="qty_update" onClick={() => {increaseQty(item.product_id)}}>+</button>
               </div>
-              <p className="price">Rs. {item.product_price}</p>
+              <p className="price">Rs. {totalPrice(item.product_id,item.product_price)}</p>
             </div>
         )
         )}
